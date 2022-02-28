5c00cfa7542a4d99fbaa49585779934639736d0e
Qihao <wuqihao07@gmail.com>
2022-02-28 13:15:28 +0800

    Testing Heroku integration

--- getdata.js
+++ getdata.js
@@ -1,7 +1,8 @@
 const b1 = document.getElementById("b1");
+const url = "https://fintech-example-app.herokuapp.com";
 b1.addEventListener("click", () => {
   let user_id = document.getElementById("inputbox").value;
-  $.getJSON(`http://localhost:3000/users/by-uid?uid=${user_id}`, (data) => {
+  $.getJSON(`${url}/users/by-uid?uid=${user_id}`, (data) => {
     let code = `First Name: ${data.first_name} <br>
                 Last Name:  ${data.last_name} <br>
                 Email:      ${data.email}`;
@@ -11,7 +12,7 @@ b1.addEventListener("click", () => {
 
 const b2 = document.getElementById("b2");
 b2.addEventListener("click", () => {
-  $.getJSON(`http://localhost:3000/users/all`, (data) => {
+  $.getJSON(`${url}/users/all`, (data) => {
     let code = "<ul>";
     data.forEach((datapoint) => {
       code += `<li> First Name: ${datapoint.first_name}
