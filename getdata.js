3f47b93740b738725f9184feca01a8f77d29af61
Qihao <wuqihao07@gmail.com>
2022-03-04 13:03:28 +0800

    connect to db

--- getdata.js
+++ getdata.js
@@ -1,8 +1,12 @@
 const b1 = document.getElementById("b1");
-const url = "https://fintech-example-app.herokuapp.com";
+// const url = "https://fintech-example-app.herokuapp.com";
+const url = "http://localhost:3000";
 b1.addEventListener("click", () => {
   let user_id = document.getElementById("inputbox").value;
   $.getJSON(`${url}/users/by-uid?uid=${user_id}`, (data) => {
+    console.log(data);
+    // data passed in as an object of a list
+    data = data[0];
     let code = `First Name: ${data.first_name} <br>
                 Last Name:  ${data.last_name} <br>
                 Email:      ${data.email}`;
@@ -13,6 +17,7 @@ b1.addEventListener("click", () => {
 const b2 = document.getElementById("b2");
 b2.addEventListener("click", () => {
   $.getJSON(`${url}/users/all`, (data) => {
+    console.log(data);
     let code = "<ul>";
     data.forEach((datapoint) => {
       code += `<li> First Name: ${datapoint.first_name}
